934f5082c1efea7ac2bbd6993c9e8a428c1c03d5
Erasmo J.F Da Silva <erasmogohiper@gmail.com>
2021-04-21 12:08:47 -0300

    CM - Correcoes

--- Elefant-Memory/Shared/Elefant_MemoryApp.swift
+++ Elefant-Memory/Shared/Elefant_MemoryApp.swift
@@ -12,7 +12,7 @@ struct Elefant_MemoryApp: App {
     let game = EmojiMemoryGame()
     var body: some Scene {
         WindowGroup {
-            ContentView(viewModel: game)
+            EmojiMemoryGameView(viewModel: game)
         }
     }
 }
--- Elefant-Memory/Shared/EmojiMemoryGameView.swift
+++ Elefant-Memory/Shared/EmojiMemoryGameView.swift
@@ -1,6 +1,6 @@
 //
-//  EmojiMemoryGameView.swift
-//  Elefant-Memory
+//  ContentView.swift
+//  Shared
 //
 //  Created by Erasmo J.F Da Silva on 21/04/21.
 //
@@ -8,53 +8,15 @@
 import SwiftUI
 
 struct EmojiMemoryGameView: View {
-  @ObservedObject  var viewModel: EmojiMemoryGame
-    
+    var viewModel: EmojiMemoryGame
     var body: some View {
-        Grid(viewModel.cards) { card in
-                CardView(card: card).onTapGesture {
-                    self.viewModel.choose(card: card)
-                }
-                .padding(5)
-            }
+        Text("Hello, world!")
             .padding()
-            .foregroundColor(.orange)
     }
 }
 
-struct CardView: View {
-    var card: MemoryGame<String>.Card
-    var body: some View {
-        GeometryReader { geometry in
-            self.body(for: geometry.size)
-      }
-    }
-   private func body(for size: CGSize) -> some View {
-        ZStack {
-            if card.isFaceUp {
-                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
-                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
-                Text(self.card.content)
-            }else{
-                if !card.isMatched {
-                     RoundedRectangle(cornerRadius: 10.0).fill()
-                    
-                }
-            }
-        }
-        .font(Font.system(size: fontSize(for: size)))
-    }
- 
-    
-   private let cornerRadius: CGFloat = 10.0
-   private let edgeLineWidth: CGFloat = 3
-   private func fontSize(for size: CGSize) -> CGFloat {
-        min(size.width, size.height) * 0.75
-    }
-}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         EmojiMemoryGameView(viewModel: EmojiMemoryGame())
     }
 }
-
