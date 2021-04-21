24a36b144984f64c6c41593dd4d40c3223c1c273
Erasmo J.F Da Silva <erasmogohiper@gmail.com>
2021-04-21 13:23:53 -0300

    CM - Create background card pie
    Create background card pie

--- Elefant-Memory/Shared/EmojiMemoryGameView.swift
+++ Elefant-Memory/Shared/EmojiMemoryGameView.swift
@@ -34,6 +34,9 @@ struct CardView: View {
             if card.isFaceUp {
                 RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
                 RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                Pie(
+                    startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90), clockwise: true)
+                    .padding(5).opacity(0.4)
                 Text(self.card.content)
             }else{
                 if !card.isMatched {
@@ -49,11 +52,13 @@ struct CardView: View {
    private let cornerRadius: CGFloat = 10.0
    private let edgeLineWidth: CGFloat = 3
    private func fontSize(for size: CGSize) -> CGFloat {
-        min(size.width, size.height) * 0.75
+        min(size.width, size.height) * 0.7
     }
 }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        EmojiMemoryGameView(viewModel: EmojiMemoryGame())
+        let game = EmojiMemoryGame()
+        game.choose(card: game.cards[0])
+       return EmojiMemoryGameView(viewModel: game)
     }
 }
